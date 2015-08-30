a6d15affdb59abfc831979da5b1de6129dfa5dd3
slavara <ratdg@ya.ru>
2015-08-30 15:57:16 +0300

    Add LayerType

---
+++ jsfl/LayerType.hx
@@ -0,0 +1,13 @@
+package jsfl;
+
+/**
+ * @author SlavaRa
+ */
+extern class LayerType {
+	public static inline var NORMAL = "normal";
+	public static inline var GUIDE = "guide";
+	public static inline var GUIDED = "guided";
+	public static inline var MASK = "mask";
+	public static inline var MASKED = "masked";
+	public static inline var FOLDER = "folder";
+}
