9d752dffb51ad6bb6fcab395ff783a9d86da6cf8
slavara <ratdg@ya.ru>
2016-03-02 17:15:39 +0300

    Minor

--- jsfl/LibraryHelper.hx
+++ jsfl/LibraryHelper.hx
@@ -1,12 +1,31 @@
 package jsfl;
 import jsfl.Library;
+using StringTools;
 
 /**
  * @author SlavaRa
  */
 class LibraryHelper {
-	
 	public static inline function getLinkageClassNames(lib:Library):Array<String> {
 		return [for(it in lib.items) if(it.linkageExportForAS) it.linkageClassName];
+	}
+	
+	public static inline function getNameToLinkageClassName(lib:Library):Map<String, String> {
+		return [for(it in lib.items) if(it.linkageExportForAS) it.name => it.linkageClassName];
+	}
+	
+	public static inline function getLinkageClassNameToName(lib:Library):Map<String, String> {
+		return [for(it in lib.items) if(it.linkageExportForAS) it.linkageClassName => it.name];
+	}
+	
+	public static inline function getLinkageClassNamesFromFolder(lib:Library, folder:String):Array<String> {
+		var linkagesToNames = getLinkageClassNameToName(lib);
+		var result = new Array<String>();
+		for(linkage in linkagesToNames.keys()) {
+			var name = linkagesToNames.get(linkage);
+			if(!name.startsWith('${folder}/')) continue;
+			result.push(linkage);
+		}
+		return result;
 	}
 }
